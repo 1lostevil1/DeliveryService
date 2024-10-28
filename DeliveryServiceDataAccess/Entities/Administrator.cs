92a33f66a8a2c20322ecee01c98515a38fc03dc3
1lostevil1 <ki-04@yandex.ru>
2024-10-28 21:24:25 +0300

    added DataAccess + some Entities

---
+++ DeliveryServiceDataAccess/Entities/Administrator.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeliveryServiceDataAccess.Entities;
+
+
+[Table("users")]
+public class Administrator : User
+{
+    [Column("admin_level")]
+    public string AdminLevel { get; set; }
+
+    [Column("department")]
+    public string Department { get; set; }
+}
---
+++ DeliveryServiceDataAccess/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeliveryServiceDataAccess.Entities;
+
+[Table("products")]
+public class Product
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("product")]
+    public string ProductName { get; set; }
+
+    [Column("cost")]
+    public decimal Cost { get; set; }
+
+    [Column("rate")]
+    public int Rating { get; set; }
+}
