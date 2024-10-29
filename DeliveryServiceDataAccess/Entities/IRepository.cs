df2c40a83eede4197ca39258e061fdffafa3c64a
1lostevil1 <ki-04@yandex.ru>
2024-10-29 22:09:09 +0300

    repoInterface

---
+++ DeliveryServiceDataAccess/Entities/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace DeliveryServiceDataAccess.Entities;
+
+public interface IRepository<T> where T: BaseEntity
+{
+    IQueryable<T> GetAllProducts();
+    IQueryable<T> GetAllOrders();
+    T? GetProductById(int id);
+    T? GetOrderById(Guid id);
+    T SaveProduct(T entity);
+    T SaveOrder(T entity);
+    T saveUser(T entity);
+    void Delete(T entity);
+}
