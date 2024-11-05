cb4de5f52437c42cbb0da8b9b7a84d057531c878
1lostevil1 <ki-04@yandex.ru>
2024-11-05 20:19:14 +0300

    added repo

--- DeliveryServiceDataAccess/Repository/IRepository.cs
+++ DeliveryServiceDataAccess/Repository/IRepository.cs
@@ -5,7 +5,6 @@ namespace DeliveryServiceDataAccess.Entities;
 public interface IRepository<T> where T : BaseEntity
 {
     IQueryable<T> GetAll();
-    IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
     T? GetById(int id);
     T? GetById(Guid id);
     T Save(T entity);
